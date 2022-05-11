80d83a138985f68491f32048bc076007fbad63cd
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-11 12:40:18 +0300

    css

---
+++ src/components/SelectedContainer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SelectedCity} from "./SelectedCity";
+import {getWeatherTC, setErrorAC} from "../redux/weather-reducer";
+import {useDispatch} from "react-redux";
+
+export const SelectedContainer = () => {
+    const dispatch = useDispatch()
+
+    const clickHandler = (title: string) => {
+        if (title.trim() === '') {
+            dispatch(setErrorAC(true))
+        } else {
+            dispatch(getWeatherTC(title, false))
+        }
+    }
+    return (
+        <div>
+            <SelectedCity callBack={clickHandler}/>
+        </div>
+    );
+};

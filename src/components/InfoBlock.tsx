a198fa3f47fb504967ea059174d0edd49e71ff99
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-13 12:02:07 +0300

    ref

--- src/components/InfoBlock.tsx
+++ src/components/InfoBlock.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-type InfoBlockType ={
+type InfoBlockType = {
     speed: number,
-    humidity:number
+    humidity: number,
+    description: string,
 }
 
- export const InfoBlock:React.FC<InfoBlockType>= ({speed,humidity}) => {
+export const InfoBlock: React.FC<InfoBlockType> = ({speed, humidity, description}) => {
     return (
 
-        <div style={{color:'grey'}}>
+        <div style={{color: 'grey'}}>
+            <div>{description}</div>
             <div>Влажность: {humidity}%</div>
             <div>Ветер: {speed}км/час</div>
         </div>
